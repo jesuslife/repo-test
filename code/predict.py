65facd532cbe66ab9a8d1a5b1f1b983855f7ddaa
Gerardo Alvarez <jesus@conceivable.life>
2024-08-13 18:50:01 -0600

    CV-2 new seed

--- code/predict.py
+++ code/predict.py
@@ -3,7 +3,7 @@
 
 # set the numpy seed for better reproducibility
 import numpy as np
-np.random.seed(100)
+np.random.seed(123)
 
 # import the necessary packages
 from torch.utils.data import DataLoader
